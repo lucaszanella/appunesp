6359fdffc4dc432a6472da901810dcd88b386361
Lucas Zanella <me@lucaszanella.com>
2018-03-28 01:27:29 -0300

    development changes

---
+++ dev/run.sh
@@ -0,0 +1,2 @@
+#docker-compose run --rm -u $( id -u $USER ):$( id -g $USER ) -v $PWD/dev/home:/home/$USER -v $HOME/Android/Sdk:/home/$USER/Android/Sdk -e USER=$USER --entrypoint "/bin/bash /my_entrypoint.sh" dev
+docker-compose run --rm -u $( id -u $USER ):$( id -g $USER ) -v $PWD/home:/home/$USER -e USER=$USER -e DISPLAY appunespdev
