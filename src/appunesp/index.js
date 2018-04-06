813de1b60407eb9fff15b6998cb6e6a1ff137f05
Lucas Zanella <me@lucaszanella.com>
2018-04-06 04:46:42 -0300

    code clean up worked!

--- src/appunesp/index.js
+++ src/appunesp/index.js
@@ -1,6 +1,8 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
-//import MessagesViewer from "./messagesViewer.js";
+//import MessagesViewer from "./messagesViewer";
+import LoginScreenView from "./LoginScreenView";
 
-AppRegistry.registerComponent('appunesp', () => App);
+//AppRegistry.registerComponent('appunesp', () => App);
 //AppRegistry.registerComponent('appunesp', () => MessagesViewer);
+AppRegistry.registerComponent('appunesp', () => LoginScreenView);
--- src/appunesp/login_screen/components/Wallpaper.js
+++ src/appunesp/login_screen/components/Wallpaper.js
@@ -1,15 +1,15 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Dimensions from 'Dimensions';
-import {StyleSheet, Image} from 'react-native';
+import { ImageBackground, StyleSheet, Image } from 'react-native';
 
 import bgSrc from '../images/wallpaper.png';
 
 export default class Wallpaper extends Component {
   render() {
     return (
-      <Image style={styles.picture} source={bgSrc}>
+      <ImageBackground style={styles.picture} source={bgSrc}>
         {this.props.children}
-      </Image>
+      </ImageBackground>
     );
   }
 }
@@ -19,6 +19,6 @@ const styles = StyleSheet.create({
     flex: 1,
     width: null,
     height: null,
-    resizeMode: 'cover',
+    //resizeMode: 'cover',
   },
 });
