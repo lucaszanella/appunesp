6086a7ee64f51dbcb972ec9ee9493f1bd9a40579
Lucas Zanella <me@lucaszanella.com>
2018-04-15 21:12:23 -0300

    makes lab more efficient

--- src/appunesp/sisgrad/sisgrad_crawler.js
+++ src/appunesp/sisgrad/sisgrad_crawler.js
@@ -184,11 +184,12 @@ export class SisgradCrawler {
         $ = r.$;
         form        = $('form[name=form_email]');
         table       = $('table', form).first().parsetable(false, false, false);
-        sender   = table[1][1];
-        subject  = table[1][2];
-        sentDate = table[1][3];
-        message = "";
-        if (table[0].length==7) { //The easiest way to detect if th table contains attachments or not
+        sender      = table[1][1];
+        subject     = table[1][2];
+        sentDate    = table[1][3];
+        message     = "";
+
+        if (table[0].length==7) { //The easiest way to detect if the table contains attachments or not, but could fail easely if something changes
           attachments = table[1][4];
           attachments = $('a', attachments).toArray();
           attachments = attachments.map(x => $(x));
@@ -199,8 +200,8 @@ export class SisgradCrawler {
                 }
           ));
           message     = clean($(table[0][5]).text());
-        } else {
-          message     = clean($(table[0][4]).text());
+        } else if (table[0].length==6){
+            message = clean($(table[0][4]).text());
         }
 
         return {
